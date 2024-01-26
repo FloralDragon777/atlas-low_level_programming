5f11a596b5490816048474167045b54cdb45853f
Emani Santos <ecses22@gmail.com>
2024-01-26 03:52:29 +0000

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit- checks for a digit.
+ * @c: int input
+ * 
+ * Return: returns 1 if digit, otherwise no.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}}
