4f0d1da8a60ba61810982ba1bed5fb05bf88faea
Emani Santos <ecses22@gmail.com>
2024-01-27 03:28:42 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,16 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * main - header for prototypes
- * void protoypes (void)
- * int protoypes (int)
- */
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int,int);
 void print_numbers(void);
-int _putchar(int c);
+int _putchar(int);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+int main(void);
+void print_triangle(int);
 
 #endif
