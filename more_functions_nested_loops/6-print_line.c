f54af7aae40b3994a4e97909e7bc7c12467caf52
Emani Santos <ecses22@gmail.com>
2024-01-27 01:09:32 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,14 @@
 #include "main.h"
 /**
- * print_line- function that checks for uppercase character.
- * @n: is the integer for the parameters of my function
- *Return: 0
+ * print_line- prints a straight line
+ * @n: parameter
+ * Return:returns to nothing
  */
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
-	{
-		while (a <= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	_putchar('\n');
-	}
-	else
-        _putchar('\n');
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
