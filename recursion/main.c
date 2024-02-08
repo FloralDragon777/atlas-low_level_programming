d3acbb4d5d1421a47df41e3d4a9fcb615c97097b
Emani Santos <ecses22@.gmail.com>
2024-02-08 19:15:43 +0000

    main.c

---
+++ recursion/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string and new line
+ *
+ * @s: inputs string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}#include "main.h"
+
+/**
+ *_puts_recursion - prints a string and new line
+ *
+ * @s: inputs string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
