dfdb4029fe460b4179003e7fcbd31e9745b07e37
Emani Santos <ecses22@gmail.com>
2024-02-11 04:48:30 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints string in reverse
+ * @s: the string that prints
+ *
+ * Return: return is void
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	int length = strlen(char *s);
+	for (int i = length - 1; i >= 0; i--)
+	{
+		putchar("%c", inputstring[i];
+	}
+	putchar("\n");
+	return "void"
+}
